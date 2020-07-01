2d7080aa6ad926e9049bac15b1edb4b973f60c82
Renan Luis <therenanluis@gmail.com>
2020-07-01 17:19:28 -0300

    Create conversor-andar.py

---
+++ conversor-andar.py
@@ -0,0 +1,5 @@
+#Conversor de andar Brasil - EUA
+andarbr = int(input('Qual o seu andar no Hotel? '))
+andareua = andarbr + 1
+
+print(f'O andar do seu quarto no hotel nos EUA é o {andareua}º andar.')
